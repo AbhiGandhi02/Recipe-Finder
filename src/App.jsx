ba9c5594eea3da40238164821256c1c29046f8c0
AbhiGandhi02 <abhi.24bcs10397@sst.scaler.com>
2025-04-21 20:01:32 +0530

    Complete App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecipeProvider } from './context/RecipeContext';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Loader from './components/ui/Loader';
+import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+
+const RecipeDetailPage = lazy(() => import('./pages/RecipeDetailPage'));
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+    return (
+        <RecipeProvider>
+            <BrowserRouter>
+                <div className="flex flex-col min-h-screen">
+                    <Navbar />
+                    <main className="flex-grow">
+                        <Suspense fallback={
+                            <div className="container mx-auto px-4 py-12 text-center">
+                                <Loader />
+                                <p className="mt-4">Loading recipe details...</p>
+                            </div>
+                        }>
+                            <Routes>
+                                <Route path="/" element={<HomePage />} />
+                                <Route path="/favorites" element={<FavoritesPage />} />
+                                <Route path="/recipe/:id" element={<RecipeDetailPage />} />
+                                <Route path="*" element={<NotFoundPage />} />
+                            </Routes>
+                        </Suspense>
+                    </main>
+                    <Footer />
+                </div>
+            </BrowserRouter>
+        </RecipeProvider>
+    );
 }
 
-export default App
+export default App;
